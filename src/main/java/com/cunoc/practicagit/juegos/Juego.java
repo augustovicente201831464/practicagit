b89bab2bcec7d0de866a5bb23340d03f3aa3bcd7
carlos vasquez <carlosvasquez201630832@cunoc.edu.gt>
2023-07-31 10:51:29 -0600

    parametro 'basketball' corregido

--- src/main/java/com/cunoc/practicagit/juegos/Juego.java
+++ src/main/java/com/cunoc/practicagit/juegos/Juego.java
@@ -7,7 +7,7 @@ package com.cunoc.practicagit.juegos;
 public abstract class Juego {
 
     public static final String AHORCADO = "ahorcado";
-    public static final String BASKETBALL = "basquetball";
+    public static final String BASKETBALL = "basketball";
     public static final String CASINO = "casino";
 
     public abstract void jugar();
